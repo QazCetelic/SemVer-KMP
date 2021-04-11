438d96fd22a21d74ea64cc921ea0ae72b66e990c
Qaz Cetelic <qaz.cetelic@protonmail.com>
2021-04-11 17:38:24 +0200

    Make the patch number optional but required if chosen to be so

--- src/commonMain/kotlin/ParseToSemver.kt
+++ src/commonMain/kotlin/ParseToSemver.kt
@@ -1,14 +1,16 @@
 package kmp_semver
 
-// Regex from https://semver.org/
-private val semVerRegex = Regex("""^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?${'$'}""")
+// Regex from https://semver.org/, but modified to add leniency so patch doesn't have to be included
+// semVerNum matches a number that can be but can't start with zero
+private val semVerNum = """0|[1-9]\d*"""
+private val semVerRegex = Regex("""^($semVerNum)\.($semVerNum)(?:\.($semVerNum))?(?:-((?:$semVerNum|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:$semVerNum|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?${'$'}""")
 
-fun String.toSemVer(): SemVer? {
+fun String.toSemVer(patchRequired: Boolean = false): SemVer? {
     val matches = semVerRegex.matchEntire(this)?.groupValues ?: return null
     return SemVer(
         major = matches[1].toIntOrNull() ?: return null,
         minor = matches[2].toIntOrNull() ?: return null,
-        patch = matches[3].toIntOrNull() ?: 0,
+        patch = matches[3].toIntOrNull() ?: if (patchRequired) return null else 0,
         preRelease = matches[4].let {
             // Sets it to null if the string is empty
             if (it.isEmpty()) null
