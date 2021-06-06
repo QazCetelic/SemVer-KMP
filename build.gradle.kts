133f255ac36b4ce20f9ad3efa0c3572285ea5ab6
Qaz Cetelic <qaz.cetelic@protonmail.com>
2021-06-06 16:20:07 +0200

    Fix gradle build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    kotlin("multiplatform") version "1.4.10"
+    kotlin("multiplatform") version "1.4.32"
 }
 
 group = "me.qaz"
-version = "1.0-SNAPSHOT"
+version = "0.2.1"
 
 repositories {
     mavenCentral()
@@ -18,17 +18,28 @@ kotlin {
             useJUnit()
         }
     }
-    js(BOTH) {
+    js(LEGACY) {
         browser {
+            commonWebpackConfig {
+                cssSupport.enabled = true
+            }
             testTask {
                 useKarma {
-                    useChromeHeadless()
-                    webpackConfig.cssSupport.enabled = true
+                    useFirefox()
                 }
             }
         }
     }
-    
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+
     sourceSets {
         val commonMain by getting
         val commonTest by getting {
@@ -49,5 +60,7 @@ kotlin {
                 implementation(kotlin("test-js"))
             }
         }
+        val nativeMain by getting
+        val nativeTest by getting
     }
 }
